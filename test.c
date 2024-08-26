00d53d85d6fb5b55ebb01c257513e85a5dab8913
fkzx8000 <117019431+fkzx8000@users.noreply.github.com>
2024-08-26 17:48:58 +0300

    Add files via upload

---
+++ test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <dlfcn.h>
+#include <string.h>
+
+
+int main(){
+     char name[50];
+printf("hi it :");
+scanf("%49s", name);
+printf(" hi : %s \n", name); 
+   return 0;
+}
+
+
+
